bb1f1a5692291db9b191a241725459851278b11a
Shruti Pathak <pathakshruti257@gmail.com>
2025-06-16 12:22:31 +0530

    tmdb api issues

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -11,7 +11,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer " + import.meta.env.VITE_REACT_APP_TMDB_KEY,
+    Authorization: "Bearer " + import.meta.env.VITE_TMDB_KEY,
   },
 };
 
